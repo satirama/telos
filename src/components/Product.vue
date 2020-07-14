c78f4793b26809b1d08d7ef01c0133630ced9062
Angelica Miranda <mirandas.angelica@gmail.com>
2020-07-14 10:01:05 -0500

    add products component & use it in store

---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="product">
+    <img src="/assets/tea_cup.jpg">
+    <p>Quantity:</p>
+    <p>Cost:</p>
+    <p>Description:</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    msg: String,
+  },
+};
+</script>
+<style scoped>
+.product {
+  width: 180px;
+  text-align: left;
+  margin: 15px
+}
+
+.product img {
+  width: 100%;
+}
+
+.product p {
+  margin: 0.5rem 0;
+  font-size: 80%;
+}
+</style>
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="store">
-
+  <div class="store container">
+    <div class="store-products content">
+      <h1>Store</h1>
+      <div class="list">
+        <Product></Product>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import Product from '@/components/Product.vue';
+
 export default {
   name: 'Store',
+  components: {
+    Product,
+  },
 };
 </script>
+<style scoped>
+.store .store-products .list {
+  display: flex;
+  flex-flow: wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
